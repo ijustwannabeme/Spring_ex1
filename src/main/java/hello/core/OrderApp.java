7ad194b210c433ebe5eaeafed3185d58d52e2f9a
liarcrown <liarcrown@naver.com>
2024-08-12 17:48:02 +0900

    remove: Order Test

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -18,6 +18,6 @@ public class OrderApp {
 
         Order order = orderService.createOrder(memberId,"itemA",10000);
         System.out.println("order = " + order);
-
+        System.out.println("order = " + order.calculatePrice());
     }
 }
